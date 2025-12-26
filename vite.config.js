2cd0035fbed172cc4fca15842af4bb9feebcbdef
Jean-Michel Grenier <jmgrenier@leslibraires.ca>
2025-12-26 10:32:42 -0500

    Move Open Graph data to Vite plugin; bump version to 1.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import htmlMinifier from 'vite-plugin-html-minifier'
 import viteImagemin from '@vheemstra/vite-plugin-imagemin'
 import cleanPlugin from 'vite-plugin-clean'
-
+import ogPlugin from 'vite-plugin-open-graph';
 import imageminMozjpeg from 'imagemin-mozjpeg'
 import imageminPngquant from 'imagemin-pngquant'
 
@@ -19,7 +19,20 @@ export default defineConfig({
                 png: imageminPngquant(),
             }
         }),
-        cleanPlugin()
+        cleanPlugin(),
+        ogPlugin({
+            basic: {
+                title: "Jean-Michel Grenier | Consultant en valorisation des données",
+                type: "website",
+                url: "https://jmgrenier.ca/",
+                image: "https://jmgrenier.ca/CFP-web-8210-BlkwTro_.jpg",
+                description: "J'aide les entreprises à prendre de meilleures décisions et à optimiser leurs processus d'affaires.",
+                locale: "fr_CA"
+            },
+            twitter: {
+                card: "summary_large_image"
+            }
+        })
     ],
     build: {
         rollupOptions: {
